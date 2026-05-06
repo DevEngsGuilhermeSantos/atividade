8115fe50a44cb1d54ed7ed99cdeb2237771377fe
DevEngsGuilhermeSantos <engs-guilhermesantos@camporeal.edu.br>
2026-05-05 21:53:52 -0300

    projeto final script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let nomes = ["Ana", "Carlos", "João", "Maria"];
 
 const lista = document.getElementById("lista");
@@ -42,5 +41,81 @@ function removerPrimeiro() {
   renderizarLista();
 }
 
+function contarMaisDe5() {
+  let contador = 0;
+
+  for (let i = 0; i < nomes.length; i++) {
+    if (nomes[i].length > 5) {
+      contador++;
+    }
+  }
+
+  alert("Quantidade de nomes com mais de 5 letras: " + contador);
+}
+
+
+
+function contarMaisDe5ForEach() {
+  let contador = 0;
+
+  nomes.forEach(nome => {
+    if (nome.length > 5) {
+      contador++;
+    }
+  });
+
+  alert("Quantidade (forEach): " + contador);
+}
+
+
+function mostrarMaiusculo() {
+  const maiusculos = nomes.map(nome => nome.toUpperCase());
+  renderizarLista(maiusculos);
+}
+
+
+
+// filtrar nomes com mais de 5 letras
+function filtrarNomes() {
+  const filtrados = nomes.filter(nome => nome.length > 5);
+  renderizarLista(filtrados);
+}
+
+// transformar todos em maiúsculo (map)
+function transformarMaiusculo() {
+  const maiusculos = nomes.map(nome => nome.toUpperCase());
+  renderizarLista(maiusculos);
+}
+
+
+function adicionarFinal() {
+  const input = document.getElementById("nomeInput");
+  if (input.value !== "") {
+    nomes.push(input.value);
+    input.value = "";
+    renderizarLista();
+  }
+}
+
+function adicionarInicio() {
+  const input = document.getElementById("nomeInput");
+  if (input.value !== "") {
+    nomes.unshift(input.value);
+    input.value = "";
+    renderizarLista();
+  }
+}
+
+function removerUltimo() {
+  nomes.pop();
+  renderizarLista();
+}
+
+function removerPrimeiro() {
+  nomes.shift();
+  renderizarLista();
+}
 
 renderizarLista();
+
+
